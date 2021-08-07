8c9dd08e3097cb9118d230b2778b1f511e701af9
Hitoshi Sakou <grahamandhitoshi@gmail.com>
2021-08-07 16:54:29 +0900

    ディレクトリ名が"/"で終わっていないときは"/"足す

--- main.js
+++ main.js
@@ -3,20 +3,21 @@
 const fs = require('fs')
 const path = require("path")
 const dirPath = process.argv[2]
-const fileNames = fs.readdirSync(dirPath)
+const newDirPath = dirPath.endsWith("/") ? dirPath : dirPath + "/"
+const fileNames = fs.readdirSync(newDirPath)
 
 const targetFileNames = fileNames.filter(RegExp.prototype.test, /.*\.jpg$|\.JPG$|\.jpeg$|\.JPEG$|\.png$|\.PNG$|\.AAE$|\.gif$|\.tif$|\.tiff$|\.HEIC$|\.MOV$|\.mov$|\.MPEG$|\.mpeg$|\.mpg$|\.MP4$|\.mp4$|\.AVI$|\.avi$|\.wmv$|\.flv$|\.mkv$/)
 
 const fullPathAndNewFullPath = targetFileNames.map((fileName) => {
-  const year = fs.statSync(dirPath + fileName).birthtime.getFullYear()
-  const month = fs.statSync(dirPath + fileName).birthtime.getMonth() + 1
-  const day = fs.statSync(dirPath + fileName).birthtime.getDate()
-  const hours = fs.statSync(dirPath + fileName).birthtime.getHours()
-  const minutes = fs.statSync(dirPath + fileName).birthtime.getMinutes()
+  const year = fs.statSync(newDirPath + fileName).birthtime.getFullYear()
+  const month = fs.statSync(newDirPath + fileName).birthtime.getMonth() + 1
+  const day = fs.statSync(newDirPath + fileName).birthtime.getDate()
+  const hours = fs.statSync(newDirPath + fileName).birthtime.getHours()
+  const minutes = fs.statSync(newDirPath + fileName).birthtime.getMinutes()
   const newName = `${year}年${month}月${day}日${hours}時${minutes}分${path.extname(fileName)}`
   return {
-    fullPath: path.join(dirPath, fileName),
-    newFullPath: path.join(dirPath, newName)
+    fullPath: path.join(newDirPath, fileName),
+    newFullPath: path.join(newDirPath, newName)
   }
 })
 
@@ -47,10 +48,11 @@ const someSerialNumFullPathAndNewFullPath = fullPathAndNewFullPath.map((file, i,
 })
 
 async function main() {
-  someSerialNumFullPathAndNewFullPath.forEach((file, index) => {
+  someSerialNumFullPathAndNewFullPath.forEach((file) => {
     console.log(file.fullPath.match(/([^/]+?)?$/)[1] + "  -->  " + file.newFullPath.match(/([^/]+?)?$/)[1])
   })
   console.log(`項目数:${someSerialNumFullPathAndNewFullPath.length}`)
+
   const {Confirm} = require('enquirer');
 
   const prompt = new Confirm({
